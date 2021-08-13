f51205e6cdbccc71d835653656022aeb4cd71875
Jason C <jforeza@gmail.com>
2021-08-13 15:31:17 -0700

    Add files via upload

---
+++ js/script-layer-test-2.js
@@ -0,0 +1,6 @@
+var CURRENT_LAYER = 2;
+var WORK_TIME_MS = 2000;
+var script_uri = "https://jasonthechiu.com/SESUPPLY-TESTADS/js/script-layer-test-3.js";
+
+// simulate some work
+doSomeWorkForIntervalThenLoad(2000, script_uri);
---
+++ js/script-layer-test-3.js
@@ -0,0 +1,5 @@
+var CURRENT_LAYER = 3;
+var WORK_TIME_MS = 2000;
+
+// simulate some work
+doSomeWorkForIntervalThenFinish(WORK_TIME_MS);
